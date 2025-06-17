14af176b74953b56592915e3034634971db805d2
Joh1707 <joaovictorvo1707@gmail.com>
2025-06-17 15:57:33 -0300

    att

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -13,11 +13,44 @@ const Usuario = sequelize.define(
       type: DataTypes.STRING(100),
       allowNull: false,
     },
+    cpf: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
     email: {
       type: DataTypes.STRING(100),
       allowNull: false,
       unique: true,
     },
+    celular: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+   endereco: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  bairro: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  cidade: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  cep: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  complemento: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  receber_ofertas: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  },
     senha: {
       type: DataTypes.STRING(255),
       allowNull: false,
